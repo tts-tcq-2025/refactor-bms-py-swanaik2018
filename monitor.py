7574ec6ad3cd108095b57c8b5ab2140406907469
swanaik2018 <swanaik2018@gmail.com>
2025-08-28 08:34:58 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,45 +1,20 @@
 # monitor.py
-import sys
-from time import sleep
 
-# --- Pure logic (functional style) ---
-def is_temperature_ok(temp):
-    return 95 <= temp <= 102
+from vitals import check_vitals
+from alerts import alert_if_not_ok
 
-def is_pulse_rate_ok(pulse):
-    return 60 <= pulse <= 100
-
-def is_spo2_ok(spo2):
-    return spo2 >= 90
-
-def check_vitals(temperature, pulseRate, spo2):
-    return {
-        "temperature": is_temperature_ok(temperature),
-        "pulse": is_pulse_rate_ok(pulseRate),
-        "spo2": is_spo2_ok(spo2)
+def vitals_ok(temperature, pulse_rate, spo2):
+    vital_values = {
+        "temperature": temperature,
+        "pulse": pulse_rate,
+        "spo2": spo2
     }
 
-# --- I/O and actions (procedural + aspect-oriented style) ---
-def blink_alert(times=6):
-    for _ in range(times):
-        print('\r* ', end='')
-        sys.stdout.flush()
-        sleep(1)
-        print('\r *', end='')
-        sys.stdout.flush()
-        sleep(1)
+    results = check_vitals(vital_values)
 
-def alert_if_not_ok(vital_name, is_ok):
-    if not is_ok:
-        print(f"{vital_name} is out of range!")
-        blink_alert()
-        return False
-    return True
+    all_ok = True
+    for vital, is_ok in results.items():
+        label = vital.replace("_", " ").capitalize()
+        all_ok = alert_if_not_ok(label, is_ok) and all_ok
 
-def vitals_ok(temperature, pulseRate, spo2):
-    results = check_vitals(temperature, pulseRate, spo2)
-    return (
-        alert_if_not_ok("Temperature", results["temperature"]) and
-        alert_if_not_ok("Pulse Rate", results["pulse"]) and
-        alert_if_not_ok("Oxygen Saturation", results["spo2"])
-    )
+    return all_ok
